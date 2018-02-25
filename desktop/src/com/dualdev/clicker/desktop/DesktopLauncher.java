78f4594b95c203b20c2b8857a458eba51944337f
brianjustice94 <brianjustice94@gmail.com>
2018-02-24 20:42:35 -0600

    Initial Commit

---
+++ desktop/src/com/dualdev/clicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dualdev.clicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dualdev.clicker.ClickerMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClickerMain(), config);
+	}
+}
